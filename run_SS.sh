e584133381221ecafa08eff5e1edd5291e2086c8
JulienAjdenbaum <julien.ajdenbaum@gmail.com>
2024-03-26 21:45:21 +0100

    first

---
+++ run_SS.sh
@@ -0,0 +1,3 @@
+python ER_superpixel_segmentation/main.py -img "/home/julien/Documents/Hyperspectral/data/jasper/jasper.mat" -sp 100 -dl 'Y'
+
+python ER_superpixel_segmentation/main.py -img "/home/julien/Documents/Hyperspectral/data/samson/samson.mat" -sp 100 -dl 'V'
